028074a80b6b260ae1bd45bafc92a5c8ed3d48a3
sabasm <smma1992@gmail.com>
2024-10-12 13:33:42 -0700

    Homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,99 +8,99 @@ const configService = new ConfigService();
 configService.loadConfig();
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+ src: "./fonts/GeistVF.woff",
+ variable: "--font-geist-sans",
+ weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+ src: "./fonts/GeistMonoVF.woff",
+ variable: "--font-geist-mono",
+ weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: configService.getSiteTitle(),
-  description: configService.getSiteDescription(),
+ title: configService.getSiteTitle(),
+ description: configService.getSiteDescription(),
 };
 
 export default function RootLayout({
-  children,
+ children,
 }: {
-  children: React.ReactNode;
+ children: React.ReactNode;
 }) {
-  const gaId = configService.getGoogleAnalyticsId();
-  const fbPixelId = configService.getFacebookPixelId();
-  const gtmId = configService.getGtmId();
+ const gaId = configService.getGoogleAnalyticsId();
+ const fbPixelId = configService.getFacebookPixelId();
+ const gtmId = configService.getGtmId();
 
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {gaId && (
-          <>
-            <Script
-              strategy="afterInteractive"
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-            />
-            <Script
-              id="google-analytics"
-              strategy="afterInteractive"
-            >
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaId}');
-              `}
-            </Script>
-          </>
-        )}
-        {gtmId && (
-          <>
-            <Script
-              id="gtm-script"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','${gtmId}');
-                `,
-              }}
-            />
-          </>
-        )}
-        {fbPixelId && (
-          <>
-            <Script
-              id="fb-pixel"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  !function(f,b,e,v,n,t,s)
-                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                  n.queue=[];t=b.createElement(e);t.async=!0;
-                  t.src=v;s=b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t,s)}(window, document,'script',
-                  'https://connect.facebook.net/en_US/fbevents.js');
-                  fbq('init', '${fbPixelId}');
-                  fbq('track', 'PageView');
-                `,
-              }}
-            />
-            <noscript>
-              <img height="1" width="1" style={{ display: 'none' }}
-                src={`https://www.facebook.com/tr?id=${fbPixelId}&ev=PageView&noscript=1`} />
-            </noscript>
-          </>
-        )}
-        {children}
-      </body>
-    </html>
-  );
+ return (
+   <html lang="en">
+     <body className={`${geistSans.variable} ${geistMono.variable}`}>
+       {gaId && (
+         <>
+           <Script
+             strategy="afterInteractive"
+             src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+           />
+           <Script
+             id="google-analytics"
+             strategy="afterInteractive"
+           >
+             {`
+               window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+               gtag('config', '${gaId}');
+             `}
+           </Script>
+         </>
+       )}
+       {gtmId && (
+         <>
+           <Script
+             id="gtm-script"
+             strategy="afterInteractive"
+             dangerouslySetInnerHTML={{
+               __html: `
+                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                 })(window,document,'script','dataLayer','${gtmId}');
+               `,
+             }}
+           />
+         </>
+       )}
+       {fbPixelId && (
+         <>
+           <Script
+             id="fb-pixel"
+             strategy="afterInteractive"
+             dangerouslySetInnerHTML={{
+               __html: `
+                 !function(f,b,e,v,n,t,s)
+                 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                 n.queue=[];t=b.createElement(e);t.async=!0;
+                 t.src=v;s=b.getElementsByTagName(e)[0];
+                 s.parentNode.insertBefore(t,s)}(window, document,'script',
+                 'https://connect.facebook.net/en_US/fbevents.js');
+                 fbq('init', '${fbPixelId}');
+                 fbq('track', 'PageView');
+               `,
+             }}
+           />
+           <noscript>
+             <img height="1" width="1" style={{ display: 'none' }}
+               src={`https://www.facebook.com/tr?id=${fbPixelId}&ev=PageView&noscript=1`} />
+           </noscript>
+         </>
+       )}
+       {children}
+     </body>
+   </html>
+ );
 }
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
+import styles from './page.module.css';
+import Link from 'next/link';
+
 export default function Home() {
-  return (
-    <>
-      work in progress...
-    </>
-  );
+ return (
+   <main className={styles.main}>
+     <h1 className={styles.title}>The Royal Game of Ur</h1>
+     <p className={styles.description}>Welcome to the ancient board game of Ur, reimagined for the digital age.</p>
+     <p className={styles.description}>Choose your game mode:</p>
+     <ul className={styles.gameModes}>
+       <li className={styles.gameMode}>
+         <Link href="/single-player" className={styles.link}>
+           Single Player
+         </Link>
+       </li>
+       <li className={styles.gameMode}>
+         <Link href="/two-player" className={styles.link}>
+           Two Player
+         </Link>
+       </li>
+       <li className={styles.gameMode}>
+         <Link href="/online-multiplayer" className={styles.link}>
+           Online Multiplayer
+         </Link>
+       </li>
+     </ul>
+     <p className={styles.description}>
+       <Link href="/about" className={styles.link}>
+         Learn the rules and history of the game in our About section.
+       </Link>
+     </p>
+   </main>
+ );
 }
+
+
